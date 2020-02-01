42d6a333969c50d144c077c5bf2f6b25a4178fe1
League Student <league@WTS14-3.attlocal.net>
2020-02-01 15:57:37 -0800

    Workshop Day 2
    Workshop Day 2

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -1,32 +1,48 @@
 package section2;
 
+import java.awt.Color;
+
 import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
 	
 	// 2. Create a new Robot
+	Robot lantern=new Robot("batman");
 
 	void go() {
 		// 4. Make the robot move as fast as possible
+		lantern.setSpeed(15000);
 
 		// 5. Set the pen width to 5
+		lantern.setPenWidth(5);
+		
 
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
+		for(int i=0;i<4;i++) {
 
 			// 7. Set the pen color to random
+			lantern.setRandomPenColor();
 	
 			// 1. Call the drawSquare() method
+			drawSquare();
 	
 			// 8. Turn the robot 90 degrees to the right
-
+			lantern.turn(90);
+		}
 	}
 
 	
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
+		lantern.penDown();
+		for(int i=0; i<100; i++)   {
+			lantern.setRandomPenColor();
+			lantern.move(100);
+			lantern.turn(90);
+			
+		}
 		
 	}
 
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -7,9 +7,14 @@ public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
 		
-		// START HERE
-	
-		
-		
+		Robot ranbir= new Robot("batman");
+ranbir.penDown();
+ranbir.setSpeed(100);
+for(int i=0; i<4; i++) {
+ranbir.turn(90);	 
+ranbir.move(100);		
+ranbir.sparkle();
+	ranbir.setPenColor(Color.ORANGE);	
+}
 	}
 }
