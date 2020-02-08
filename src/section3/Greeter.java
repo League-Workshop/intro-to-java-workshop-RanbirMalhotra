bd63c18927817fb9512c26102ddce9e2b6cc84ad
League Student <league@WTS14-3.attlocal.net>
2020-02-08 15:55:51 -0800

    Workshop day 3

---
+++ src/section3/Greeter.java
@@ -0,0 +1,10 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class Greeter {
+public static void main(String[] args) {
+	String answer= JOptionPane.showInputDialog("Whats your name?");
+			System.out.println(answer);
+}
+}
---
+++ src/section3/NicestProgram.java
@@ -0,0 +1,13 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class NicestProgram {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, ("Something Nice"));
+	JOptionPane.showMessageDialog(null, ("Nice"));
+JOptionPane.showMessageDialog(null, ("Ice"));
+String answer= JOptionPane.showInputDialog("How are you?");
+System.out.println (answer);
+}
+}
